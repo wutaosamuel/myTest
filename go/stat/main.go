750150d2cac94280c73e747571a95af0d1f78fe1
wutao <497863986@qq.com>
2020-04-14 14:59:01 +0800

    fix time

--- go/stat/main.go
+++ go/stat/main.go
@@ -14,4 +14,6 @@ func main() {
 	}
 	fmt.Println(f.ModTime())
 	fmt.Println(time.Now())
+	fmt.Println(f.ModTime().Add(5*time.Minute))
+	fmt.Println(time.Now().Before(f.ModTime().Add(5*time.Minute)))
 }
