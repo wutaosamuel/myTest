2a4fac2895a83b81266295bccf44f1bdd02e7b52
wutao <497863986@qq.com>
2020-09-18 13:56:01 +0800

    done leetcode

--- cpp/leetcode/p50.cpp
+++ cpp/leetcode/p50.cpp
@@ -5,71 +5,35 @@
 
 using namespace std;
 
-vector<vector<string>> groupAnagrams(vector<string> &strs)
+double myPow(double x, int n)
 {
-  if (strs.size() == 0)
-    return vector<vector<string>>{{}};
-  vector<vector<string>> result = {vector<string>{strs[0]}};
-  for (int i = 1; i < strs.size(); i++)
+  bool positive;
+  int positiven;
+  double result;
+
+  if (n == 0)
+    return 1;
+  if (x == 0)
+    return 0;
+  if (n == 1)
+    return x;
+
+  positive = (n >= 0) ? true : false;
+  positiven = (positive) ? n : -n;
+  result = 1;
+
+  for (int i = 1; i <= positiven; i <<= 1)
   {
-    bool canRow = true;
-    for (int j = 0; j < result.size(); j++)
-    {
-      string rowfirst = result[j][0];
-      string tmp = "";
-      for (int k = 0; k < rowfirst.size(); k++)
-      {
-        if (strs[i].find(rowfirst.substr(k, 1)) != string::npos)
-        {
-          tmp += rowfirst.substr(k, 1);
-        }
-        else
-        {
-          tmp = "";
-          canRow = true;
-          break;
-        }
-      }
-      cout << "rowfirst: " << rowfirst << endl;
-      cout << "tmp: " << tmp << endl;
-      cout << i << ": " << strs[i] << endl;
-      if (rowfirst == tmp && rowfirst != "")
-      {
-        result[j].push_back(strs[i]);
-        canRow = false;
-        break;
-      }
-      if (rowfirst == tmp && strs[i] != "")
-      {
-        canRow = true;
-        break;
-      }
-      if (rowfirst == tmp && strs[i] == "")
-      {
-        result[j].push_back(strs[i]);
-        canRow = false;
-        break;
-      }
-    }
-    if (canRow)
-    {
-      vector<string> res = {strs[i]};
-      result.push_back(res);
-    }
+    cout << i << endl;
+    if ((i & positiven) == i)
+      result *= x;
+    x *= x;
   }
-  return result;
+
+  return (positive) ? result : double(1) / result;
 }
 
 int main()
 {
-  //vector<string> vec = {"","dans"};
-  vector<string> vec = {"and","dans"};
-  int i = 0;
-  for (auto a : groupAnagrams(vec))
-  {
-    cout << "array " << i << ": " << endl;
-    i++;
-    for (string s : a)
-      cout << s << endl;
-  }
+  cout << myPow(2.00, -2) << endl;
 }
