48bc39243c16a0ab080ef401373b9aedf13e9219
wutao <497863986@qq.com>
2021-05-24 14:01:57 +0800

    done typescript

---
+++ typescript/array/concat.ts
@@ -0,0 +1,12 @@
+let array1 = ["first", "array"];
+let array2 = ["second", "array"];
+let s = "single word";
+
+console.log("concat 2 array: ");
+console.log(array1.concat(array2));
+console.log("array1:", array1);
+console.log();
+
+console.log("concat array and world: ");
+console.log(array1.concat(s));
+console.log();
