67d835f31a3da53f7d1eaded95e42c94ea8103f3
wutao <497863986@qq.com>
2021-08-25 16:00:44 +0800

    refine postgre and done schema

--- go/postgresql/base/main.go
+++ go/postgresql/base/main.go
@@ -8,149 +8,6 @@ import (
 	_ "github.com/lib/pq"
 )
 
-func main() {
-	pqInfo := NewPQTest()
-	pq_info := pqInfo.Source()
-	db, err := sql.Open("postgres", pq_info)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	db.SetMaxOpenConns(2000)
-	db.SetMaxIdleConns(1000)
-	defer db.Close()
-	// db.Ping()
-	// create table if not exist
-	tx, err := db.Begin()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println("if create not exists, create table")
-	f, err := ioutil.ReadFile("./test_object_if_exists.sql")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	table := string(f)
-	if _, err := tx.Exec(table); err != nil {
-		fmt.Println(err)
-		return
-	}
-	tx.Commit()
-
-	// Insert
-	tx, err = db.Begin()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println()
-	fmt.Println("Start Insert")
-	o := NewObject()
-	o.Name = "Object"
-	o.ID = 1
-	o.Value = 12.123456789
-	key, value := o.InsertTo()
-	insert := "INSERT INTO test_object(" + key + ") VALUES (" + value + ");"
-	fmt.Println(insert)
-	if _, err := tx.Exec(insert); err != nil {
-		fmt.Println(err)
-		return
-	}
-	tx.Commit()
-
-	// query
-	tx, err = db.Begin()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println()
-	fmt.Println("Start to query")
-	q := NewObject()
-	query := "SELECT * FROM test_object WHERE name = 'Object';"
-	row := tx.QueryRow(query)
-	if err := row.Scan(q.SelectTo()...); err != nil {
-		fmt.Println(err)
-		return
-	}
-	tx.Commit()
-	fmt.Println(q)
-
-	// update
-	tx, err = db.Begin()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println()
-	fmt.Println("Start to update")
-	o.Value = 8.9876654321
-	update := "UPDATE test_object SET " + o.UpdateTo() + "WHERE name = 'Object';"
-	if _, err := tx.Exec(update); err != nil {
-		fmt.Println(err)
-		return
-	}
-	q = NewObject()
-	row = tx.QueryRow(query)
-	if err := row.Scan(q.SelectTo()...); err != nil {
-		fmt.Println(err)
-		return
-	}
-	tx.Commit()
-	fmt.Println(q)
-
-	// delete
-	tx, err = db.Begin()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println()
-	fmt.Println("Start to delete")
-	delete := "DELETE FROM test_object WHERE name = 'Object'"
-	if _, err := tx.Exec(delete); err != nil {
-		fmt.Println(err)
-		return
-	}
-	// allquery
-	o.Name = "obj"
-	key, value = o.InsertTo()
-	insert = "INSERT INTO test_object(" + key + ") VALUES (" + value + ");"
-	if _, err := tx.Exec(insert); err != nil {
-		fmt.Println(err)
-		return
-	}
-	o.Name = "obj_d"
-	o.ID = 2
-	key, value = o.InsertTo()
-	insert = "INSERT INTO test_object(" + key + ") VALUES (" + value + ");"
-	if _, err := tx.Exec(insert); err != nil {
-		fmt.Println(err)
-		return
-	}
-	allquery := "SELECT * FROM test_object;"
-	rows, err := tx.Query(allquery)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	for rows.Next() {
-		a := NewObject()
-		if err := rows.Scan(a.SelectTo()...); err != nil {
-			fmt.Println(err)
-			return
-		}
-		fmt.Println(a)
-	}
-	if err := rows.Err(); err != nil {
-		fmt.Println(err)
-		return
-	}
-	tx.Commit()
-}
-
 // PQTest
 type PQDb struct {
 	Host     string
@@ -180,4 +37,194 @@ func (pq *PQDb) Source() string {
 // Insert
 func Insert(key, value string) string {
 	return ""
+}
+
+func main() {
+	pqInfo := NewPQTest()
+	pq_info := pqInfo.Source()
+	db, err := sql.Open("postgres", pq_info)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	db.SetMaxOpenConns(2000)
+	db.SetMaxIdleConns(1000)
+	defer db.Close()
+	// db.Ping()
+	// create scheme/table if not exist
+	createST(db)
+
+	// Insert
+	o := NewObject()
+	o.Name = "Object"
+	o.ID = 1
+	o.Value = 12.123456789
+	insert(db, o)
+
+	// query
+	query(db)
+
+	// update
+	o.Value = 8.9876654321
+	update(db, o)
+
+	// delete
+	delete(db)
+	// allquery
+	tx, err := db.Begin()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	o.Name = "obj"
+	key, value := o.InsertTo()
+	insert := "INSERT INTO test_object(" + key + ") VALUES (" + value + ");"
+	if _, err := tx.Exec(insert); err != nil {
+		fmt.Println(err)
+		return
+	}
+	o.Name = "obj_d"
+	o.ID = 2
+	key, value = o.InsertTo()
+	insert = "INSERT INTO test_object(" + key + ") VALUES (" + value + ");"
+	if _, err := tx.Exec(insert); err != nil {
+		fmt.Println(err)
+		return
+	}
+	tx.Commit()
+	allquery(db)
+}
+
+// create scheme/table if not exist
+func createST(db *sql.DB) {
+	// create scheme/table if not exist
+	tx, err := db.Begin()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("if create not exists, create table")
+	f, err := ioutil.ReadFile("./test_object_if_exists.sql")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	table := string(f)
+	if _, err := tx.Exec(table); err != nil {
+		fmt.Println(err)
+		return
+	}
+	tx.Commit()
+}
+
+// insert
+func insert(db *sql.DB, obj *Object) {
+	// Insert
+	tx, err := db.Begin()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println()
+	fmt.Println("Start Insert")
+	key, value := obj.InsertTo()
+	insert := "INSERT INTO test_object(" + key + ") VALUES (" + value + ");"
+	fmt.Println(insert)
+	if _, err := tx.Exec(insert); err != nil {
+		fmt.Println(err)
+		return
+	}
+	tx.Commit()
+}
+
+// query
+func query(db *sql.DB) {
+	// query
+	tx, err := db.Begin()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println()
+	fmt.Println("Start to query")
+	q := NewObject()
+	query := "SELECT * FROM test_object WHERE name = 'Object';"
+	row := tx.QueryRow(query)
+	if err := row.Scan(q.SelectTo()...); err != nil {
+		fmt.Println(err)
+		return
+	}
+	tx.Commit()
+	fmt.Println(q)
+}
+
+// update
+func update(db *sql.DB, obj *Object) {
+	// update
+	tx, err := db.Begin()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println()
+	fmt.Println("Start to update")
+	update := "UPDATE test_object SET " + obj.UpdateTo() + "WHERE name = 'Object';"
+	if _, err := tx.Exec(update); err != nil {
+		fmt.Println(err)
+		return
+	}
+	// read whether exists
+	q := NewObject()
+	query := "SELECT * FROM test_object WHERE name = 'Object';"
+	row := tx.QueryRow(query)
+	if err := row.Scan(q.SelectTo()...); err != nil {
+		fmt.Println(err)
+		return
+	}
+	tx.Commit()
+	fmt.Println(q)
+}
+
+// delete
+func delete(db *sql.DB) {
+	tx, err := db.Begin()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println()
+	fmt.Println("Start to delete")
+	delete := "DELETE FROM test_object WHERE name = 'Object'"
+	if _, err := tx.Exec(delete); err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
+// allquery
+func allquery(db *sql.DB) {
+	tx, err := db.Begin()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	allquery := "SELECT * FROM test_object;"
+	rows, err := tx.Query(allquery)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	for rows.Next() {
+		a := NewObject()
+		if err := rows.Scan(a.SelectTo()...); err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println(a)
+	}
+	if err := rows.Err(); err != nil {
+		fmt.Println(err)
+		return
+	}
+	tx.Commit()
 }
--- go/postgresql/base/test_object_if_exists.sql
+++ go/postgresql/base/test_object_if_exists.sql
@@ -1,4 +1,6 @@
-CREATE TABLE IF NOT EXISTS "test_object" (
+CREATE SCHEMA IF NOT EXISTS test_schema;
+
+CREATE TABLE IF NOT EXISTS test_schema.test_object (
 	"name" varchar(255) PRIMARY KEY,
 	"id" int NOT NULL,
 	"value" double precision DEFAULT 0
