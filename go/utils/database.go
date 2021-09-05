de860b023dae8002a340eb9e989016341d140517
wutao <497863986@qq.com>
2021-09-05 22:02:23 +0800

    done database and done test

---
+++ go/utils/database.go
@@ -0,0 +1,283 @@
+package main
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"reflect"
+	"strings"
+)
+
+// Database
+type Database struct {
+	Host     string
+	Port     int
+	User     string
+	Password string
+	DBName   string
+}
+
+// NewDatabase
+func NewDatabase() *Database {
+	return &Database{
+		Host:     "",
+		Port:     5432,
+		User:     "",
+		Password: "",
+		DBName:   "",
+	}
+}
+
+// Set
+func (db *Database) Set(host string, port int, user, password, db_name string) {
+	db.Host = host
+	db.Port = port
+	db.User = user
+	db.Password = password
+	db.DBName = db_name
+}
+
+// Source
+// postgre
+func (db *Database) Source() string {
+	return fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		db.Host, db.Port, db.User, db.Password, db.DBName)
+}
+
+// ToSchema
+// postgre
+func ToSchema(schema string) string {
+	return "CREATE SCHEMA IF NOT EXISTS " + schema + ";"
+}
+
+// ToSchemaTable
+// postgre
+func ToSchemaTable(schema, table, elements string) string {
+	t := schema + "." + table
+	if table == "" {
+		return ""
+	}
+	if schema == "" {
+		t = table
+	}
+	sql := "CREATE TABLE IF NOT EXISTS " + t + `(
+	` + elements + `
+	);`
+	return sql
+}
+
+// CreateSchema
+func CreateSchema(DB *sql.DB, schema string) error {
+	tx, err := DB.Begin()
+	if err != nil {
+		return err
+	}
+	defer tx.Commit()
+
+	schemaSQL := ToSchema(schema)
+	if _, err := tx.Exec(schemaSQL); err != nil {
+		return err
+	}
+	return nil
+}
+
+// CreateSchemaTable
+func CreateSchemaTable(DB *sql.DB, schema, table, elements string) error {
+	tx, err := DB.Begin()
+	if err != nil {
+		return err
+	}
+	defer tx.Commit()
+
+	tableSQL := ToSchemaTable(schema, table, elements)
+	if _, err := tx.Exec(tableSQL); err != nil {
+		return err
+	}
+	return nil
+}
+
+// Insert
+/**
+ *	Insert INSERT INTO table(column1, column2 ...) VALUE ($1, $2, ...);
+ *		, 'value1', value2, ...
+ *  @Params
+ *    - DB  			database
+ *		- table     schema.table
+ *		- new 			object{field:value}
+ **/
+func InsertDB(DB *sql.DB, table string, new_s []interface{}) error {
+	tx, err := DB.Begin()
+	if err != nil {
+		return err
+	}
+	defer tx.Commit()
+
+	for _, new := range new_s {
+		keys, values, args, err := ToInsertSQL(new)
+		if err != nil {
+			return err
+		}
+
+		insert := "INSERT INTO " + table + "(" + keys + ") VALUES (" + values + ");"
+		if _, err := tx.Exec(insert, args...); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// Query
+/**
+ *	Query SELECT * FROM table condition;
+ *	NOTE: select * can fit to model field
+ *  @Params
+ *    - DB  				database
+ *		- table     	schema.table
+ *		- condition		e.g. WHERE name = 'name' AND value >= 10; "" will get all values in table
+ *		- new 				object{field:value}
+ *	@Returns
+ *		- []object{field:value}
+ *		- error
+ **/
+func QueryDB(DB *sql.DB, table, condition string, model interface{}) ([]interface{}, error) {
+	result := make([]interface{}, 0)
+	tx, err := DB.Begin()
+	if err != nil {
+		return result, err
+	}
+	defer tx.Commit()
+
+	modelType := reflect.TypeOf(model)
+	if modelType.Kind() == reflect.Ptr {
+		modelType = modelType.Elem()
+	}
+	if modelType.Kind() != reflect.Struct {
+		return result, errors.New("query model type is not struct")
+	}
+
+	query := "SELECT * FROM " + table + " " + condition + ";"
+	if condition == "" {
+		query = "SELECT * FROM " + table + ";"
+	}
+	rows, err := tx.Query(query)
+	if err != nil {
+		return result, err
+	}
+	for rows.Next() {
+		object := reflect.New(modelType).Elem()
+		numField := object.NumField()
+		column := make([]interface{}, numField)
+		for i := 0; i < numField; i++ {
+			field := object.Field(i)
+			column[i] = field.Addr().Interface()
+		}
+		if err := rows.Scan(column...); err != nil {
+			return result, err
+		}
+		result = append(result, object.Addr().Interface())
+	}
+	if err := rows.Err(); err != nil {
+		return result, err
+	}
+	return result, nil
+}
+
+// Update
+/**
+ * Update UPDATE table SET column1 = 'value1', column2 = value2 ... condition;
+ * @Params
+ *   - DB  					database
+ *	 - table     		schema.table
+ *	 - condition		e.g. WHERE name = 'name' AND value >= 10
+ *	 - new 					object{field:value}
+ **/
+func UpdateDB(DB *sql.DB, table, condition string, new map[string]interface{}) error {
+	tx, err := DB.Begin()
+	if err != nil {
+		return err
+	}
+	defer tx.Commit()
+
+	updateColumns, args, err := ToUpdateSQL(new)
+	if err != nil {
+		return err
+	}
+	update := "UPDATE " + table + " SET " + updateColumns + " " + condition + ";"
+	if _, err := tx.Exec(update, args...); err != nil {
+		return err
+	}
+	return nil
+}
+
+// Delete
+/**
+ * Delete DELETE FROM table condition;
+ * @Params
+ *   - DB  					database
+ *	 - table     		schema.table
+ *	 - condition		e.g. WHERE name = 'name' AND value >= 10
+ **/
+func DeleteDB(DB *sql.DB, table, condition string) error {
+	tx, err := DB.Begin()
+	if err != nil {
+		return err
+	}
+	defer tx.Commit()
+
+	delete := "DELETE FROM " + table + " " + condition + ";"
+	if _, err := tx.Exec(delete); err != nil {
+		return err
+	}
+	return nil
+}
+
+// ToInsertSQL
+func ToInsertSQL(new interface{}) (string, string, []interface{}, error) {
+	keys := ""
+	values := ""
+	args := make([]interface{}, 0)
+	t := reflect.TypeOf(new)
+	v := reflect.ValueOf(new)
+	if t.Kind() == reflect.Ptr {
+		t = t.Elem()
+		v = v.Elem()
+	}
+	if t.Kind() != reflect.Struct {
+		return keys, values, args, errors.New("object not struct in update sql")
+	}
+	numField := t.NumField()
+	for i := 0; i < numField-1; i++ {
+		field := t.Field(i)
+		value := v.Field(i)
+		keys += fmt.Sprintf("%s, ", strings.ToLower(field.Name))
+		values += fmt.Sprintf("$%d, ", i+1)
+		args = append(args, value.Interface())
+	}
+	//last one
+	field := t.Field(numField - 1)
+	value := v.Field(numField - 1)
+	keys += strings.ToLower(field.Name)
+	values += fmt.Sprintf("$%d", numField)
+	args = append(args, value.Interface())
+	return keys, values, args, nil
+}
+
+// ToUpdateSQL return column1 = 'value', column2 = 12.123
+func ToUpdateSQL(new map[string]interface{}) (string, []interface{}, error) {
+	sql := ""
+	args := make([]interface{}, 0)
+	i := 0
+	for k, v := range new {
+		// last one
+		if i == len(new)-1 {
+			sql += fmt.Sprintf("%s = $%d", strings.ToLower(k), i+1)
+			args = append(args, v)
+			return sql, args, nil
+		}
+
+		i += 1
+		sql += fmt.Sprintf("%s = $%d, ", strings.ToLower(k), i)
+		args = append(args, v)
+	}
+	return sql, args, nil
+}
---
+++ go/utils/database_test.go
@@ -0,0 +1,174 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+func TestCreateSchema(t *testing.T) {
+	sql := ToSchema("test_schema")
+	t.Log("TestCreateSchema  --> pass")
+	t.Log(sql)
+}
+
+func TestCreateSchemaTable(t *testing.T) {
+	elements := `"name" varchar(255) PRIMARY KEY,
+	"id" int NOT NULL,
+	"value" double precision DEFAULT 0`
+	sql := ToSchemaTable("test_schema", "test_object", elements)
+	t.Log("TestCreateSchemaTable  --> pass")
+	t.Log(sql)
+}
+
+func TestInsertSQL(t *testing.T) {
+	// Object
+	type Object struct {
+		Name  string
+		ID    int
+		Value float64
+	}
+	k := "name, id, value"
+	v := "$1, $2, $3"
+	a := []interface{}{"object", -1, 12.123456789}
+	object := &Object{"object", -1, 12.123456789}
+
+	keys, values, args, err := ToInsertSQL(object)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log("TestInsertSQL  --> pass")
+	t.Log(k, "()needs to equal to()", keys)
+	if k != keys {
+		t.Fatal("keys not correct")
+	}
+	t.Log(v, "()needs to equal to()", values)
+	if v != values {
+		t.Fatal("values not correct")
+	}
+	t.Log(a, "()needs to equal to()", args)
+	for i := 0; i < len(args); i ++ {
+		if a[i] != args[i] {
+			t.Fatal("args not correct")
+		}
+	}
+}
+
+func TestUpdateSQL(t *testing.T) {
+	// Object
+	u := "id = $1, value = $2, name = $3"
+	a	:= []interface{}{-1, 12.123456789, "object"}
+	object := map[string]interface{}{
+		"name":  "object",
+		"id":    -1,
+		"value": 12.123456789,
+	}
+	update, args, err := ToUpdateSQL(object)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log("TestUpdateSQL  --> pass")
+	// NOTE: map cause out of order, it should be ok
+	t.Log(u, "()needs to equal to()", update)
+	t.Log(a, "()needs to equal to()", args)
+}
+
+func TestDatabase(t *testing.T) {
+	// Object
+	type Object struct {
+		Name  string
+		ID    int
+		Value float64
+	}
+	schema := "test_schema"
+	table := "test_object"
+	schemaTable := fmt.Sprintf("%s.%s", schema, table)
+	elements := `"name" varchar(255) PRIMARY KEY,
+	"id" int NOT NULL,
+	"value" double precision DEFAULT 0`
+	database := NewDatabase()
+	database.Set("172.16.8.221", 5432, "pi", "wutaowutao", "test")
+	db, err := sql.Open("postgres", database.Source())
+	if err != nil {
+		t.Fatal(err)
+	}
+	db.SetMaxOpenConns(2000)
+	db.SetMaxIdleConns(1000)
+
+	t.Log()
+	t.Log("create schema  --> pass")
+	if err := CreateSchema(db, "test_schema"); err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log("create schema.table  --> pass")
+	if err := CreateSchemaTable(db, schema, table, elements); err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log("insert  --> pass")
+	zero := &Object{"zero", 0, 12.123456789}
+	first := &Object{"first", 1, 12.123456789}
+	second := &Object{"second", 2, 2.123456789}
+	object := &Object{"object", -1, 0.123456789}
+	if err := InsertDB(db, schemaTable, []interface{}{zero, first, second, object}); err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log("query --> pass")
+	if results, err := QueryDB(db, schemaTable, "WHERE name = 'zero'", &Object{}); err != nil {
+		t.Fatal(err)
+	} else {
+		for i, result := range results {
+			t.Log("The query result", i, result)
+		}
+	}
+
+	t.Log("update --> pass")
+	changed := map[string]interface{}{
+		"name":  "o",
+		"value": 1.1,
+	}
+	if err := UpdateDB(db, schemaTable, "WHERE id = -1", changed); err != nil {
+		t.Fatal(err)
+	}
+	if results, err := QueryDB(db, schemaTable, "WHERE id = -1", &Object{}); err != nil {
+		t.Fatal(err)
+	} else {
+		for i, result := range results {
+			t.Log("The query result", i, result)
+		}
+	}
+
+	t.Log("query all --> pass")
+	if results, err := QueryDB(db, schemaTable, "", &Object{}); err != nil {
+		t.Fatal(err)
+	} else {
+		for i, result := range results {
+			t.Log("all query result", i, ": ", result)
+		}
+	}
+
+	t.Log("delete all one by one --> pass")
+	if err := DeleteDB(db, schemaTable, "WHERE name = 'zero'"); err != nil {
+		t.Fatal(err)
+	}
+	if err := DeleteDB(db, schemaTable, "WHERE name = 'first'"); err != nil {
+		t.Fatal(err)
+	}
+	if err := DeleteDB(db, schemaTable, "WHERE name = 'second'"); err != nil {
+		t.Fatal(err)
+	}
+	if err := DeleteDB(db, schemaTable, "WHERE id = -1"); err != nil {
+		t.Fatal(err)
+	}
+	if results, err := QueryDB(db, schemaTable, "", &Object{}); err != nil {
+		t.Fatal(err)
+	} else {
+		if len(results) != 0 {
+			t.Fatal("not delete all yet")
+		}
+	}
+}
