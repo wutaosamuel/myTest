de4df61cd28a2170abd987ce6e29c57e500b3a79
wutao <497863986@qq.com>
2021-09-19 20:25:02 +0800

    add called cache

---
+++ go/cache/called/CalledCache.go
@@ -0,0 +1,248 @@
+package utils
+
+import (
+	"runtime"
+	"sync"
+	"time"
+)
+
+const (
+	DefaultFrequency = int64(60)
+	DefaultCycleTime = time.Hour
+)
+
+// Cache
+type CalledCache struct {
+	*cache
+}
+
+// Item
+type Item struct {
+	Object interface{}
+}
+
+// ItemCounter
+type ItemCounter struct {
+	Value int64
+	mu    *sync.RWMutex
+}
+
+// cash
+type cache struct {
+	cycleTime time.Duration
+	frequency int64
+	items     map[string]*Item
+	counter   map[string]*ItemCounter
+
+	stop chan bool
+	mu   *sync.RWMutex
+}
+
+// Set
+func (c *cache) Set(key string, value interface{}) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if item, found := c.items[key]; found {
+		item.set(value)
+		return
+	}
+	item := NewItem(value)
+	counter := NewItemCounter()
+	item.set(value)
+	c.items[key] = item
+	c.counter[key] = counter
+}
+
+// set
+func (i *Item) set(value interface{}) {
+	i.Object = value
+}
+
+// Get
+func (c *cache) Get(key string) (interface{}, bool) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+
+	return c.get(key)
+}
+
+// get
+func (c *cache) get(key string) (interface{}, bool) {
+	if item, found := c.items[key]; found {
+		counter := c.counter[key]
+		counter.called()
+		return item.Object, true
+	}
+	return nil, false
+}
+
+// called
+func (ic *ItemCounter) called() {
+	ic.mu.Lock()
+	defer ic.mu.Unlock()
+
+	ic.Value += 1
+}
+
+// GetKeys
+func (c *cache) GetKeys() []string {
+	c.mu.RLock()
+	defer c.mu.Unlock()
+
+	keys := make([]string, len(c.items))
+	for k := range c.items {
+		keys = append(keys, k)
+	}
+	return keys
+}
+
+// Add
+func (c *cache) Add(key string, value interface{}) bool {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if _, found := c.items[key]; found {
+		return false
+	}
+	item := NewItem(value)
+	counter := NewItemCounter()
+	item.set(value)
+	c.items[key] = item
+	c.counter[key] = counter
+	return true
+}
+
+// Delete
+func (c *cache) Delete(key string) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	delete(c.items, key)
+	delete(c.counter, key)
+}
+
+// Flush
+func (c *cache) Flush() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.items = make(map[string]*Item)
+	c.counter = make(map[string]*ItemCounter)
+}
+
+// Items
+func (c *cache) Items() map[string]Item {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+
+	items := make(map[string]Item)
+	for k, v := range c.items {
+		items[k] = *v
+	}
+	return items
+}
+
+// ItemCount
+func (c *cache) ItemCount() int {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+
+	return len(c.items)
+}
+
+// DeleteNoneFrequency
+func (c *cache) DeleteNoneFrequency() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	for k, v := range c.counter {
+		if v.Value < c.frequency {
+			delete(c.counter, k)
+			delete(c.items, k)
+			break
+		}
+		// reset counter
+		v.Value = 0
+	}
+}
+
+// ResetCounter
+func (c *cache) ResetCounter() {
+	for _, v := range c.counter {
+		v.ResetCounter()
+	}
+}
+
+// ResetCounter
+func (ic *ItemCounter) ResetCounter() {
+	ic.mu.Lock()
+	defer ic.mu.Unlock()
+
+	ic.Value = 0
+}
+
+// NewItem
+func NewItem(object interface{}) *Item {
+	return &Item{
+		Object: object,
+	}
+}
+
+// NewItemCounter
+func NewItemCounter() *ItemCounter {
+	return &ItemCounter{
+		Value: 0,
+		mu:    &sync.RWMutex{},
+	}
+}
+
+// newcache
+func newcache(cycleTime time.Duration, frequency int64) *cache {
+	cTime := DefaultCycleTime
+	f := DefaultFrequency
+	if cycleTime != 0 {
+		cTime = cycleTime
+	}
+	if frequency != 0 {
+		f = frequency
+	}
+
+	return &cache{
+		cycleTime: cTime,
+		frequency: f,
+		items:     make(map[string]*Item),
+		counter:   make(map[string]*ItemCounter),
+
+		stop: make(chan bool),
+		mu:   &sync.RWMutex{},
+	}
+}
+
+// run
+func (c *cache) run() {
+	ticker := time.NewTicker(c.cycleTime)
+	defer ticker.Stop()
+	for {
+		select {
+		case <-ticker.C:
+			c.DeleteNoneFrequency()
+		case <-c.stop:
+			return
+		}
+	}
+}
+
+// stop
+func stop(C *CalledCache) {
+	C.cache.stop <- true
+}
+
+// New
+func NewCalledCache(defaultCycleTime time.Duration, defaultFrequency int64) *CalledCache {
+	c := newcache(defaultCycleTime, defaultFrequency)
+	C := &CalledCache{c}
+
+	go c.run()
+	runtime.SetFinalizer(C, stop)
+
+	return C
+}
---
+++ go/cache/called/CalledCache_test.go
@@ -0,0 +1,116 @@
+package utils
+
+import (
+	"testing"
+	"time"
+)
+
+const isDebug = false
+
+func TestCache(t *testing.T) {
+	var found bool
+	cc := NewCalledCache(time.Minute, 10)
+
+	itemLen := cc.ItemCount()
+	if itemLen != 0 {
+		t.Error("Just stat, it should 0 items")
+	}
+
+	cc.Set("a", 1)
+	cc.Set("b", "b")
+
+	a, found := cc.Get("a")
+	if !found {
+		t.Error("Already set a element, should be found")
+	}
+	if a == nil {
+		t.Error("a value is 1, not nil")
+	}
+	// a_value, ok = a.(int)
+	if a_value := a.(int); a_value+2 != 3 {
+		t.Error("a value is 1 (int), 1 + 2 should be equal 3")
+	}
+	debugLog(t, a)
+
+	b, found := cc.Get("b")
+	if !found {
+		t.Error("Already set a element, should be found")
+	}
+	if b == nil {
+		t.Error("a value is b, not nil")
+	}
+	if b_value := b.(string); b_value+"B" != "bB" {
+		t.Error("a value is 1 (int), 1 + 2 should be equal 3")
+	}
+	debugLog(t, b)
+}
+
+func TestCacheTime(t *testing.T) {
+	cc := NewCalledCache(time.Minute, 8)
+
+	cc.Set("a", 1)
+	cc.Set("b", "b")
+	cc.Set("Del", "delete")
+
+	aGet := func() {
+		a, found := cc.Get("a")
+		if !found {
+			t.Error("Already set a element, should be found")
+		}
+		if a == nil {
+			t.Error("a value is 1, not nil")
+		}
+		debugLog(t, a)
+	}
+	bGet := func() {
+		b, found := cc.Get("b")
+		if !found {
+			t.Error("Already set a element, should be found")
+		}
+		if b == nil {
+			t.Error("a value is b, not nil")
+		}
+		debugLog(t, b)
+	}
+	cGet := func() {
+		c, found := cc.Get("Del")
+		if !found {
+			t.Error("Already set a element, should be found")
+		}
+		if c == nil {
+			t.Error("c value is delete, not nil")
+		}
+		debugLog(t, c)
+	}
+
+	for i := 0; i <= 20; i++ {
+		go aGet()
+
+		if i&1 == 0 {
+			go bGet()
+		}
+
+		if i%3 == 0 {
+			go cGet()
+		}
+	}
+
+	<-time.After(time.Minute)
+
+	aGet()
+	bGet()
+	// should not get c now
+	c, found := cc.Get("Del")
+	if found {
+		t.Error("Del(c) key, should not get value")
+	}
+	if c != nil {
+		t.Error("c value should be delete, should be nil now")
+	}
+}
+
+func debugLog(t *testing.T, args ...interface{}) {
+	if isDebug {
+		t.Log(args...)
+	}
+}
